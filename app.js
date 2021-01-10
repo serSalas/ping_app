7a3f5364ec2dab3fb451ae8379c4a502e2b278ef
serSalas <sergiosalas@mi.unc.edu.ar>
2021-01-10 09:09:29 -0300

    add app.js

--- app.js
+++ app.js
@@ -2,5 +2,33 @@ const https = require("https");
 
 const opciones = {
 	hostname: process.env.TARGET,
-	method: process.env.METHOD
+	metodo: process.env.METHOD
 };
+
+console.log(
+	"** web ping ** Respuesta desde %s , metodo:%s, tiempo= %dms",
+	opciones.hostname,
+	opciones.metodo,
+	process.env.INTERVAL
+);
+
+let i = 1;
+let start = new Date().getTime();
+setInterval(() => {
+	start = new Date().getTime();
+	console.log(" Numero de request: %d; a las %d", i++, start);
+	var req = https.request(opciones, res => {
+		var end = new Date.getTime();
+		var duracion = end - start;
+		console.log(
+			"Estado de respuesta: %s a las %d; duracion: %dms",
+			res.statusCode,
+			end,
+			duracion
+		);
+		req.on("error", e => {
+			console.error(e);
+		});
+		req.end();
+	}, process.env.INTERVAL)
+
